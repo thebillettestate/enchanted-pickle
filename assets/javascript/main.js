32f94cb5571c3c9743d196893586a72602bab481
smellyalater <acopleman@gmail.com>
2015-11-10 00:02:48 -0500

    prettified files

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,24 +1,23 @@
 $(document).ready(function() {
 
-var pickleToCart = $('div.caption h3:eq(0)').text();
-var picklesToCart = $('div.caption h3:eq(1)').text();
-var hoodieToCart = $('div.caption h3:eq(2)').text();
+    var pickleToCart = $('div.caption h3:eq(0)').text();
+    var picklesToCart = $('div.caption h3:eq(1)').text();
+    var hoodieToCart = $('div.caption h3:eq(2)').text();
 
-	$('.btn.btn-success:eq(0)').click(function() {
-		event.preventDefault();
-		console.log(pickleToCart);
-		$('.cart-item-1').after('<p>' + pickleToCart + '</p>');
-	});
+    $('.btn.btn-success:eq(0)').click(function() {
+        event.preventDefault();
+        console.log(pickleToCart);
+        $('.cart-item-1').after('<p>' + pickleToCart + '</p>');
+    });
 
-	$('.btn.btn-success:eq(1)').click(function() {
-		event.preventDefault();
-		$('.cart-item-1').after('<p>' + picklesToCart + '</p>');
-	});
+    $('.btn.btn-success:eq(1)').click(function() {
+        event.preventDefault();
+        $('.cart-item-1').after('<p>' + picklesToCart + '</p>');
+    });
 
-	$('.btn.btn-success:eq(2)').click(function() {
-		event.preventDefault();
-		$('.cart-item-1').after('<p>' + hoodieToCart + '</p>');
-	});
+    $('.btn.btn-success:eq(2)').click(function() {
+        event.preventDefault();
+        $('.cart-item-1').after('<p>' + hoodieToCart + '</p>');
+    });
 
 });
-
